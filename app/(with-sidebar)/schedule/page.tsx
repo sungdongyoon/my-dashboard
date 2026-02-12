347701851a14279b716897fc1ea2af2be7f202d8
sungdongyoon <ehdtjd0906@naver.com>
2026-02-12 23:14:16 +0900

    schedule 페이지 FullCalendar 세팅

--- app/(with-sidebar)/schedule/page.tsx
+++ app/(with-sidebar)/schedule/page.tsx
@@ -1,38 +1,90 @@
 "use client";
 
 import { Calendar, CalendarDayButton } from "@/components/ui/calendar";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
 import React, { useState } from "react";
 
+interface ScheduleType {
+  id: number;
+  date: string;
+  content: string;
+}
+
 const Schedule = () => {
-  const [date, setDate] = useState<Date | any>(new Date());
+  const [date, setDate] = useState<string | any>("");
+  const [schedule, setSchedule] = useState<ScheduleType[]>([
+    {
+      id: 1,
+      date: "2026-02-03",
+      content: "오늘은 2월 3일~",
+    },
+    {
+      id: 2,
+      date: "2026-02-11",
+      content: "오늘은 2월 11일~",
+    },
+    {
+      id: 3,
+      date: "2026-02-15",
+      content: "오늘은 2월 15일~",
+    },
+    {
+      id: 4,
+      date: "2026-02-20",
+      content: "오늘은 2월 20일~",
+    },
+    {
+      id: 5,
+      date: "2026-02-22",
+      content: "오늘은 2월 22일~",
+    },
+    {
+      id: 6,
+      date: "2026-02-03",
+      content: "오늘은 2월 3일~ !!",
+    },
+  ]);
+  const [selectedDate, setSelectedDate] = useState<string | null>(null);
+
   return (
-    <div className="bg-red-100 w-full h-full">
-      <Calendar
+    <div className="w-full h-full">
+      <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" />
+      {/* <Calendar
         mode="single"
         selected={date}
         onSelect={setDate}
-        className="rounded-lg border"
+        className="w-full rounded-lg border"
+        classNames={{
+          months: "calendar_months",
+          month: "calendar_month",
+          weekdays: "calendar_weekdays bg-blue-100",
+          week: "calendar_week",
+          day: "calendar_day border aspect-auto h-auto",
+          day_button: "aspect-auto",
+        }}
         components={{
-          DayButton: ({ children }) => {
-            return <div>{children}test</div>;
+          DayButton: ({ children, day, modifiers, ...props }) => {
+            return (
+              <CalendarDayButton
+                day={day}
+                modifiers={modifiers}
+                {...props}
+                onClick={() => setSelectedDate(day.isoDate)}
+              >
+                <div className="w-full">{children}</div>
+                <ul className="w-full bg-red-50">
+                  {schedule
+                    .filter((el) => el.date === day.isoDate)
+                    .map((el) => (
+                      <li key={el.id}>{el.content}</li>
+                    ))}
+                </ul>
+              </CalendarDayButton>
+            );
           },
         }}
-        // components={{
-        //   DayButton: ({ children, modifiers, day, ...props }) => {
-        //     const isWeekend =
-        //       day.date.getDay() === 0 || day.date.getDay() === 6;
-
-        //     return (
-        //       <CalendarDayButton day={day} modifiers={modifiers} {...props}>
-        //         {children}
-        //         {!modifiers.outside && (
-        //           <span>{isWeekend ? "$120" : "$100"}</span>
-        //         )}
-        //       </CalendarDayButton>
-        //     );
-        //   },
-        // }}
-      />
+      /> */}
     </div>
   );
 };
